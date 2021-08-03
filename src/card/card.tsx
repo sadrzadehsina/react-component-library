2550daf001a3e5b7454825aa8b4240c2e93026c9
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2021-08-03 20:28:28 +0430

    Add dark classes to card

--- src/card/card.tsx
+++ src/card/card.tsx
@@ -9,10 +9,12 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 export const Card: FC<Props> = ({ renderHeader, renderBody, renderFooter }) => {
   return (
-    <div className="bg-gray-50 border border-gray-100">
+    <div className="bg-gray-50 dark:bg-gray-800">
       <div className="header">{renderHeader()}</div>
-      <div className="body p-4">{renderBody()}</div>
-      <div className="footer p-4 border-t-2 border-white">{renderFooter()}</div>
+      <div className="body p-4 dark:text-white">{renderBody()}</div>
+      <div className="footer p-4 border-t-2 border-white dark:border-gray-700">
+        {renderFooter()}
+      </div>
     </div>
   );
 };
